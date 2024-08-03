a567b59c2ba8fa06836a5a742042bcd51e3cd5e7
altunf <altunnfirat@gmail.com>
2024-08-04 01:41:14 +0300

    feature: Added authentication logic

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,9 +1,52 @@
+import User from "../models/userModel.js";
+import bcrypt from "bcrypt";
+
 const registerController = async (req, res) => {
   try {
-  } catch (error) {}
+    const { username, password, email } = req.body;
+    const userCheck = await User.findOne(email);
+
+    if (userCheck) {
+      return res.status(500).json({ msg: "kullanıcı mevcut" });
+    }
+
+    const passwordHash = await bcrypt.hash(password, 12);
+    const newUser = User.create({ username, email, password: passwordHash });
+
+    const token = await jwt.sign(
+      { userId: newUser._id },
+      process.env.JWT_SECRET,
+      { expiresIn: "1h" }
+    );
+
+    res.status(201).json({ status: "OK", newUser, token });
+  } catch (error) {
+    return res.status(500).json({ msg: error.msg });
+  }
 };
 
 const loginController = async (req, res) => {
   try {
-  } catch (error) {}
+    const { username, password, email } = req.body;
+    const userCheck = await User.findOne(email);
+    if (!userCheck) {
+      return res.status(500).json({ msg: "kullanıcı bulunamadı" });
+    }
+
+    const passwordCompare = await bcrypt.compare(password, userCheck.password);
+    if (!passwordCompare) {
+      return res.status(500).json({ msg: "Şifre Yanlış" });
+    }
+    const token = await jwt.sign(
+      { userId: userCheck._id },
+      process.env.JWT_SECRET,
+      { expiresIn: "1h" }
+    );
+
+    res.status(200).json({ status: "OK", userCheck, token });
+  } catch (error) {
+    return res.status(500).json({ msg: error.msg });
+  }
 };
+
+export default { registerController, loginController };
--- server/models/userModel.js
+++ server/models/userModel.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import validator from "validator";
 
 const { Schema } = mongoose;
 
@@ -8,16 +9,23 @@ const userSchema = new Schema(
       type: String,
       required: [true, "Username area is required"],
       lowercase: true,
+      trim: true,
+      validate: [validator.isAlphanumeric, "Only Alphanumeric characters"],
     },
     email: {
       type: String,
       required: [true, "Email area is required"],
       unique: true,
+      validate: [validator.isEmail, "Valid email is required"],
     },
     password: {
       type: String,
       required: [true, "Password area is required"],
       minLength: [4, "At least 4 characters"],
+    },
+    date: {
+      type: Date,
+      default: new Date(),
     },
   },
   {
