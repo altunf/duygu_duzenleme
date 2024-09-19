658266b4ddc51bace5c44dfe0e2057ab66d720fd
altunf <altunnfirat@gmail.com>
2024-09-20 00:25:04 +0300

    feat: created task controller

---
+++ server/controllers/taskController.js
@@ -0,0 +1,44 @@
+import { Task } from "../models/taskModel.js";
+
+const newTaskController = async (req, res) => {
+  try {
+    const { title, mood, date } = req.body;
+
+    const userID = req.headers["current-user"];
+
+    const newTask = await Task.create({
+      title,
+      mood,
+      date,
+      userID: userID,
+    });
+
+    res.status(201).json({ status: "OK", newTask });
+  } catch (error) {
+    console.error("Egzersiz eklenirken bir hata oluştu:", error);
+    return res.status(500).json({ msg: "Egzersiz eklenirken bir hata oluştu" });
+  }
+};
+
+const deleteTaskController = async (req, res) => {
+  const id = req.headers["Task-id"];
+  try {
+    const deletedTask = await Task.findByIdAndDelete(id);
+    res.status(200).json(deletedTask);
+  } catch (error) {
+    res.status(500).json({ message: "Error deleting tasks", error });
+  }
+};
+
+const getAllTasksController = async (req, res) => {
+  const id = req.headers["current-user"];
+
+  try {
+    const tasks = await Task.find({ userID: id });
+    res.status(200).json(tasks);
+  } catch (error) {
+    res.status(500).json({ message: "Error fetching tasks", error });
+  }
+};
+
+export { newTaskController, getAllTasksController, deleteTaskController };
